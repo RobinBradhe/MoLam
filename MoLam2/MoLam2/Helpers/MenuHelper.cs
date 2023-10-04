52d9dde9500ab3ca15725ede2e69208ac0275a13
Robin Brådhe <robin.bradhe@netsolution.se>
2023-10-04 12:38:02 +0200

    Menu added

---
+++ MoLam2/MoLam2/Helpers/MenuHelper.cs
@@ -0,0 +1,30 @@
+﻿using MoLam2.Models.ViewModels;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace MoLam2.Helpers
+{
+    public static class MenuHelper
+    {
+
+        public static async Task<IEnumerable<NavigationModel>> GetNavigationModels(this IEnumerable<IPublishedContent> originalList)
+        {
+            if (!originalList.Any())
+                return Enumerable.Empty<NavigationModel>();
+
+            var nav = new List<NavigationModel>();
+            foreach (var item in originalList.Where(x => x.IsVisible()))
+            {
+                nav.Add(new NavigationModel()
+                {
+                    Name = item!.Name,
+                    Title = item!.Name,
+                    Url = item.Url()
+                });
+
+
+            }
+            return await Task.FromResult(nav);
+
+        }
+    }
+}
---
+++ MoLam2/MoLam2/Models/ViewModels/NavigationModel.cs
@@ -0,0 +1,23 @@
+﻿using MoLam2.Helpers;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace MoLam2.Models.ViewModels
+{
+    public class NavigationModel
+    {
+        public NavigationModel()
+        {
+        }
+
+        public NavigationModel(IEnumerable<IPublishedContent> publishedContents)
+        {
+            ChildItems = publishedContents.GetNavigationModels();
+        }
+
+        public Task<IEnumerable<NavigationModel>> ChildItems { get; set; }
+
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+    }
+}
